414694e2afcdb99d7846193a24e567ef7a818481
JalavadiNaveen <jalavadinaveen1999@gmail.com>
2022-03-20 17:52:37 +0000

    foodmenupart

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,19 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './admin/homepage/homepage.component';
 import { AddmenuComponent } from './admin/addmenu/addmenu.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SignupComponent } from './signup/signup.component';
 import { GetmenuComponent } from './admin/getmenu/getmenu.component';
 import { DeletemenuComponent } from './admin/deletemenu/deletemenu.component';
 import { EditmenuComponent } from './admin/editmenu/editmenu.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
     HomepageComponent,
     GetmenuComponent,
     AddmenuComponent,
